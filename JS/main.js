b0cf41c91fe19e543885a797c8c76999faaa65ab
Sarath Sasi <sarathsasi.g@gmail.com>
2023-02-05 16:17:20 +0530

    basic structure created

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+let question = document.getElementById("quiz-question-1");
+
+let option1 = document.getElementById("quiz-answer-1");
+let option2 = document.getElementById("quiz-answer-2");
+let option3 = document.getElementById("quiz-answer-3");
+let option4 = document.getElementById("quiz-answer-4");
+
+question.innerText = "question";
+option1.innerText = "test1";
+option2.innerText = "test2";
+option3.innerText = "test3";
+option4.innerText = "test4";
