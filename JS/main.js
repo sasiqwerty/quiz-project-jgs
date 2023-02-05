4de15496665a235fef0215e8a1c705671c44b57c
Sarath Sasi <sarathsasi.g@gmail.com>
2023-02-05 17:09:31 +0530

    basic colors and shape of the site

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,19 @@
+// variable declaration for the question
 let question = document.getElementById("quiz-question-1");
-
+// variable declarations for answers
 let option1 = document.getElementById("quiz-answer-1");
 let option2 = document.getElementById("quiz-answer-2");
 let option3 = document.getElementById("quiz-answer-3");
 let option4 = document.getElementById("quiz-answer-4");
+// variable declaration for the score output
+let score = document.getElementById("score-output");
 
+//output
 question.innerText = "question";
-option1.innerText = "test1";
-option2.innerText = "test2";
-option3.innerText = "test3";
-option4.innerText = "test4";
+option1.innerText = "answer1";
+option2.innerText = "answer2";
+option3.innerText = "answer3";
+option4.innerText = "answer4";
+score.innerText = "10";
+
+// Find a way to link a json file and let it iterate over the questions and answers
